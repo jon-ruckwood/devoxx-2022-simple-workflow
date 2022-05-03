6d6ee1f6e4756a3ffbc4e56970fd3b9a0286cc0a
Jonathan Ruckwood <jon-ruckwood@users.noreply.github.com>
2022-05-03 10:14:33 +0100

    Add simple test
    Update maven plugins.

---
+++ src/test/java/net/selfdotlearn/helloworld/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package net.selfdotlearn.helloworld;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloWorldTest {
+
+    @Test
+    void shouldAlwaysBeTrue() {
+        assertThat(true).isTrue();
+    }
+}
